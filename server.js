216143dd530f811d89fbe360a28adfc2dd98b10b
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-08-01 22:34:01 -0700

    Adding socket

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const path = require ('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+const { Socket } = require('dgram');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Static Folder//
 app.use(express.static(path.join(__dirname, 'public')));
 
+// run when client connects///
+ io.on('connection', socket => {
+     console.log('New WS connection..');
+ })
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
