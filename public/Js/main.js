d3127fcbc1481a5cdfa7a580f1ef1f06ff6b8197
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-08-01 23:02:31 -0700

    Server,mainjs, index.html

--- public/Js/main.js
+++ public/Js/main.js
@@ -1,1 +1,6 @@
 const socket = io();
+
+
+socket.on('message', message => {
+    console.log('message');
+});
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require ('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const { Socket } = require('dgram');
 
+//
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -13,8 +13,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // run when client connects///
  io.on('connection', socket => {
-     console.log('New WS connection..');
- })
+     console.log('New WS connection...')
+        // welcome user to chat//
+     socket.emit('message', 'Welcome to PetesCord!');
+
+     //Broadcast when a user Connects//
+     socket.broadcast.emit('message','A new person has joined our chat! welcome them!');
+     // Runs when client disconnects//
+     socket.on('disconnect', () => {
+        io.emit('message', 'Somone left the building!');
+     });
+
+ });
 
 const PORT = 3000 || process.env.PORT;
 
