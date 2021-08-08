0a89878d2bea05838ae80720293e8ac8d6b7c889
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-08-08 02:34:36 -0700

    Added more icons

--- server.js
+++ server.js
@@ -1,53 +1,75 @@
-const path = require ('path');
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage= require('./utils/messages');
-const {userJoin, getCurrentUser} = require('./utils/users');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
 
-//
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-//Static Folder//
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord bot';
+const botName = 'PetesCord Bot';
 
-// run when client connects///
- io.on('connection', socket => {
-     socket.on('joinRoom',({username, room}) => {
-        const user = userJoin( socket.id,  username, room);
-         
-        socket.join(user.room);
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
+    socket.join(user.room);
 
-        // welcome current user to chat//
-     socket.emit('message', formatMessage(botName,'Welcome to Petes Cord'));
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to Petes Cord!'));
 
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the Cord`)
+      );
 
-     //Broadcast when a user Connects//
-     socket.broadcast
-     .to(user.room)
-     .emit('message',formatMessage(botName,'FRESH MEAT IN THE ROOM!'));
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
 
-     });
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
 
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
 
-     //listen for chat message//
-     socket.on('chatMessage',(msg) => {
-         io.emit('message',formatMessage('USER',msg));
-     });
-
-  // Runs when client disconnects//
+  // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName,'Somone left the building!'));
- });
+    const user = userLeave(socket.id);
 
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the cord`)
+      );
 
- });
-//// run localport300////
-const PORT = 3000 || process.env.PORT;
+      // Send users and room info
+      io.to(user.room).emit('RoomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
 
-server.listen(PORT, () => console.log(`All GOOD HERE FULL STACK P! ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`ALL GOOD FULL STACK  P! ${PORT}`));
