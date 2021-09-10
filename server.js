29a4053313d623b3e8b5430a82e04fdbcd19de37
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-09-09 22:43:14 -0700

    Adding sql Db

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to Petes Cord!'));
+    socket.emit('message', formatMessage(botName, 'Welcome to Net Cord!'));
 
     // Broadcast when a user connects
     socket.broadcast
